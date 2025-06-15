5e13bffaceec3cb324e6016e375baf6f682ddea8
arnoldae9 <arnold_a_e@hotmail.com>
2025-06-14 22:43:42 -0600

    Fix:funciones: se recortarorn las funciones.

--- link_bio/link_bio/views/App_To_Do/to_do_app.py
+++ link_bio/link_bio/views/App_To_Do/to_do_app.py
@@ -9,11 +9,9 @@ estados = ["eliminada", "pendiente", "completada"]
 """funciones"""
 def open_csv(archivo_csv:str=db_local_path, first_time:bool=True):
     """función para abrir el archivo csv local
-
     Args:
         archivo_csv (csv): archivo csv db_local.csv
         first_time (bool, optional): se usa para definir cuando se manda llamar por primera vez. Defaults to True.
-
     Returns:
         pd.DataFrame: regresa un data frame con las tareas almacenadas. 
     """    
@@ -30,9 +28,9 @@ def archivo_csv_existe(archivo_csv):
     """verifica la existencia del archivo"""
     return Path(archivo_csv).exists()
 
-def create_csv(nombre="link_bio/views/App_To_Do/bd_local.csv"):
-    """funcion para crear el archivo si es que no existe"""
-    data_default = {"id": [1], "tarea": ["Mirar mi Web"], "estado": ["completada"]}
+def create_csv(nombre=db_local_path):
+    """función para crear el archivo si es que no existe"""
+    data_default = {"id": [1], "tarea": ["Mirar mi Web"], "estado": ["pendiente"]}
     df = pd.DataFrame(data_default)
     df.to_csv(nombre, index=False, encoding="utf-8")
     return print("db_local.csv creado ...")
@@ -49,16 +47,19 @@ def new_task(task: str):
     save_csv(df)
     return print(f"Nueva tarea agregada: {nuevo_registro[1]}")
 
-def view_pending_tasks(archivo_csv):
-    """Leer las tareas pendientes desde el archivo csv"""
-    pass
+def view_pending_tasks():
+    """Leer las tareas pendientes desde el archivo csv, la idea es que regrese un data frame filtrado"""
+    archivo_csv=open_csv(first_time=False)
+    return archivo_csv[archivo_csv["estado"]=="pendiente"]
+    
 
-def view_completed_tasks(archivo_csv):
-    """Leer las tareas eliminadas desde el archivo csv"""
-    pass
+def view_completed_tasks():
+    """Leer las tareas completadas desde el archivo csv, el objetivo es mostrar las completadas"""
+    archivo_csv=open_csv(first_time=False)
+    return archivo_csv[archivo_csv["estado"]=="completada"]
 
-def save_csv(new_data_frame, archivo_csv=db_local_path):
-    new_data_frame.to_csv(archivo_csv, index=False, encoding='utf-8')
+def save_csv(new_db_local, archivo_csv=db_local_path):
+    new_db_local.to_csv(archivo_csv, index=False, encoding='utf-8')
 
 def complete_task(task):
     df = open_csv(first_time=False)
@@ -67,10 +68,10 @@ def complete_task(task):
     
 if __name__ == "__main__":
     open_csv()
-    
-    #TODO: cuando se cree el botón check in en la app no habrá un input, la idea es que tome el str del mismo botón.
-    #task_new = input("Ingresa la nueva tarea: ")
-    #new_task(task_new)
-    tarea_completada = str(input(f"introduzca la tarea a eliminar: "))
-    complete_task(tarea_completada)
-
+    # TODO: cuando se cree el botón check in en la app no habrá un input, la idea es que tome el str del mismo botón.
+    # task_new = input("Ingresa la nueva tarea: ")
+    # new_task(task_new)
+    # tarea_completada = str(input(f"introduzca la tarea a eliminar: "))
+    # complete_task(tarea_completada)
+    # view_pending_tasks()
+    view_completed_tasks()
